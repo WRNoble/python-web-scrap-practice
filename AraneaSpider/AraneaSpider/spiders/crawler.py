85369973d5535938b2ebe1b8fe800813317cf7f7
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-10-09 11:09:58 -0400

    improvements made

--- AraneaSpider/AraneaSpider/spiders/crawler.py
+++ AraneaSpider/AraneaSpider/spiders/crawler.py
@@ -1,1 +1,19 @@
 from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+class HorseSpider(CrawlSpider):
+
+    name = 'Whirlaway'
+
+    allowed_domains = ['treehouse_projects.github.io']
+
+    start_urls = ['https://treehouse-projects.github.io/horse-land']
+
+    rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_horses', follow=True)]
+
+    def parse_horses(self, response):
+        url = response.url
+        title = response.css('title::text').extract()
+        print('Page URL: {}'.format(url))
+        print('PAge title: {}'.format(title))
+
